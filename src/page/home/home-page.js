3a1fa6280a96ec17539e7147b689e114c40c8dd8
yuQ-7148 <1159915745@qq.com>
2021-09-28 13:17:28 +0800

    first commit

---
+++ src/page/home/home-page.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+class HomePage extends React.Component {
+    render() {
+        return(
+            <div>
+                this is antV
+            </div>
+        )
+    }
+}
+
+export default HomePage;
